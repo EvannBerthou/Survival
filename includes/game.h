1baca6f6a33440e3b386cbe9d07fd62b8ef73eea
Sutalite <nedje28@gmail.com>
2020-12-19 22:58:31 +0100

    Boucle d'event

--- includes/game.h
+++ includes/game.h
@@ -4,8 +4,14 @@
 
 struct Game {
     SDL_Window *window;
+    SDL_Renderer *renderer;
+
+    bool running = true;
 
     void init();
     void run();
+    void event(SDL_Event *event);
+    void update();
+    void render();
     void close();
 };
--- src/game.cpp
+++ src/game.cpp
@@ -3,28 +3,42 @@
 
 void Game::init() {
     sec(SDL_Init(SDL_INIT_VIDEO), "Error while initializing SDL");
-    window = sec(SDL_CreateWindow("Survival", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
+    window = sec(SDL_CreateWindow("Survival", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
                 800, 600, SDL_WINDOW_SHOWN),
                 "Error while creating window");
-    SDL_Renderer *renderer = sec(SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED),
+    renderer = sec(SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED),
                                 "Error while creating renderer");
-    (void) renderer;
 }
 
 void Game::run() {
-    SDL_Event event;
-    bool running = true;
+    SDL_Event ev;
     while(running) {
-        while (SDL_PollEvent(&event)) {
-            switch (event.type) {
-            case SDL_QUIT: running = false; break;
-            }
+        event(&ev);
+        update();
+        render();
+    }
+}
+
+void Game::event(SDL_Event *event) {
+    while (SDL_PollEvent(event)) {
+        switch (event->type) {
+        case SDL_QUIT: running = false; break;
         }
     }
 }
 
+void Game::update() {
+}
+
+void Game::render() {
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+}
+
 
 void Game::close() {
-    std::cout << "Closing game" << std::endl;
     free(window);
+    SDL_Quit();
+    std::cout << "Closing game" << std::endl;
 }
